0dee31b81484f900a8d6ccd2f8f1470f1a61cda8
MichaelJOHare <mjohare13@gmail.com>
2024-02-08 00:57:29 -0500

    Responsive design mostly implemented, just small adjustments to be made details in main.js.

--- chess/main.js
+++ chess/main.js
@@ -5,13 +5,16 @@ console.log("GameController initialized", gameController);
 
 /*
 TO DO
- - Move-history wrap extending box into game-container/chessboard for some reason
+ - Fix draggingdiv location incorrect after resizing window
+
+ - Fix Move history scrollbar when viewport really tall and skinny before media query
+        - move next/prev move buttons in small screen media query (fix move history being under text areas too)
  - Debug possible memory leak (might be extension react dev tools or stockfish idk yet)
 
  - Go through and change private fields to normal, remove getters.
      -Thought it was a good idea, not liking it now.  Maybe keep setters?  They make sense in my brain
  - Make move piece with click a smooth glide to location
-      - Add onTouchMove etc.
+      - Add onTouchMove, maybe sounds, etc.
  - Implement drawing arrows/circles with right click
  - Implement stockfish/flip board/misc UI (maybe PGN box)
 */
